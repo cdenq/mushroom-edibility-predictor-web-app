518b4093e763d0bf8e925d02c2740aba0df941ea
PlanetM <ermcgrew@users.noreply.github.com>
2022-03-02 21:07:28 -0500

    small edits to html and flask files

--- app.py
+++ app.py
@@ -14,6 +14,18 @@ def index():
 def prediction():
     data = predict.make_predictions()
     return jsonify(data)
+
+@app.route('/mushroom_facts')
+def facts():
+    return render_template('mushroom_facts.html')
+
+@app.route('/compare_models')
+def models():
+    return render_template('compare_models.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
    
 if __name__ == "__main__":
     app.run(debug=True)
