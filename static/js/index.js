1961374cec809d3eac18536950aaf6e582ff18e2
PlanetM <ermcgrew@users.noreply.github.com>
2022-03-07 15:39:30 -0500

    validation for user input

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,19 @@
 async function prediction(){
     //build object to pass
     let feature_list = {};
-
+    //all user input elements
     let selections = document.getElementsByClassName('mushroom_traits');
     for (let i = 0; i < selections.length; i++) {
-        feature_list[selections[i].id] = selections[i].value
+        let selection = selections[i].value;
+        //filter for traits that user did not input
+        if (selection.includes('Select')){
+            feature_list[selections[i].id] = '0'; 
+        } else {
+            feature_list[selections[i].id] = selection;
+        };
     };
 
-    console.log(feature_list)
-    //feature_list in correct order for python fnc--or build that as part of the python function?
-    
-    //how to handle default "select xx" values (if user doesn't input answer for every field)
-    //submit button inside form--how to make it call this function 
-    
-
+    // console.log(feature_list);
 
     //call flask route
     let response = await fetch('/predict', {
