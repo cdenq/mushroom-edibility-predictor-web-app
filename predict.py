29d71f1e58e6b0eca7c2d0106830f1848c535743
PlanetM <ermcgrew@users.noreply.github.com>
2022-03-08 21:38:02 -0500

    working on js population of results

--- predict.py
+++ predict.py
@@ -1,45 +1,52 @@
-import joblib
-import os
-from supervised_ml import score, variables
+# import joblib
+# import os
+# from supervised_ml import score, variables
 
-#save models into a list
-model_results = {'modelName': [],
-                'modelPrediction': [],
-                'modelAccuracy': []}
+# #save models into a list
+# model_results = {'modelName': [],
+#                 'modelPrediction': [],
+#                 'modelAccuracy': []}
 
-for key, file in enumerate(os.listdir('Main/Resources/Models')):
-    filename = os.fsdecode(file)
-    print(filename)
-    model_name = filename.split(sep = '.')[0]
-    print(model_name)
-    model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
-    model = joblib.load('Main/Resources/Models/' + filename)
-    model_results['modelName'].append(model_list[key])
-    model_results['modelPrediction'].append('y')
-    model_results['modelAccuracy'].append(50)
+# for key, file in enumerate(os.listdir('Main/Resources/Models')):
+#     filename = os.fsdecode(file)
+#     print(filename)
+#     model_name = filename.split(sep = '.')[0]
+#     print(model_name)
+#     model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
+#     model = joblib.load('Main/Resources/Models/' + filename)
+#     model_results['modelName'].append(model_list[key])
+#     model_results['modelPrediction'].append('y')
+#     model_results['modelAccuracy'].append(50)
  
-print(model_results)
+# print(model_results)
 
-model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
-model_scores = []
+# model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
+# model_scores = []
+
+# def make_predictions(data):
+
+#     model_results = {'modelName': [],
+#                     'modelPrediction': [],
+#                     'modelAccuracy': []}
+
+#     X = []
+#     print(data)
+
+
+#     for key, file in enumerate(os.listdir('Main/Resources/Models')):
+#         filename = os.fsdecode(file)
+#         model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
+#         model = joblib.load('Main/Resources/Models/' + filename)
+#         prediction = model.predict(data)
+#         model_results['modelName'].append(model_list[key])
+#         model_results['modelPrediction'].append(prediction[0])
+#         model_results['modelAccuracy'].append('placeholder')
+
+#     return model_results, data
 
 def make_predictions(data):
-
-    model_results = {'modelName': [],
-                    'modelPrediction': [],
-                    'modelAccuracy': []}
-
-    X = []
-    print(data)
-
-
-    for key, file in enumerate(os.listdir('Main/Resources/Models')):
-        filename = os.fsdecode(file)
-        model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
-        model = joblib.load('Main/Resources/Models/' + filename)
-        prediction = model.predict(data)
-        model_results['modelName'].append(model_list[key])
-        model_results['modelPrediction'].append(prediction[0])
-        model_results['modelAccuracy'].append('placeholder')
-
-    return model_results, data
+    if (data):
+        model_results = {'modelName': ['Extremely Random Forest'],
+                    'modelPrediction': ["edible"],
+                    'modelAccuracy': [89]}
+        return(model_results)
--- static/js/index.js
+++ static/js/index.js
@@ -27,6 +27,38 @@ async function prediction(){
     });
     let data = await response.json();
 
+    // test code to make sure function is working
+    // document.querySelector("#predictions").textContent = data;
+
+    // remove old th data
+
     //populate html with data
-    document.querySelector("#predictions").textContent = data;
+    // table with model name & prediction
+    post_data = document.querySelector('tbody');
+    const newtr = document.createElement('tr');
+    const newth = document.createElement('th');
+   
+
+    //add rows per @ results received--this is always the same tho?
+    //for (i=0;i<data.length;i++):
+   
+    data.map(item => {
+        let newth = document.createElement('th');
+        newth.textContent = item;
+        newth.id = 'results';
+        document.querySelector('tr').appendChild.newth
+    })
+
+    // newth.textContent = data.modelName;
+    // post_data.append(newth);
+//    if edible: <td class="table-success">...</td>
+//   if poisonous: <td class="table-danger">...</td>
+
+    
+
+
+
+
+
+    // chart of accuracy scores--will this be the same every time? 
 };
