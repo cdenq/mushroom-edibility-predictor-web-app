7ae3cbbb53b14821967db4e9a8bbf800686893f8
Benjamin <bmxnguyen@gmail.com>
2022-03-10 00:05:55 -0500

    inputted data working

--- predict.py
+++ predict.py
@@ -3,32 +3,10 @@ import os
 from main_preprocessing import preprocessing
 import supervised_ml
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
 initial_data = preprocessing()[0]
-
-# save models into a list
-# model_results = {'modelName': [],
-#                 'modelPrediction': [],
-#                 'modelAccuracy': [],
-#                 'modelF1': [],
-#                 'modelPrecision': [],
-#                 'modelRecall': []}
-
-# for key, file in enumerate(os.listdir('Main/Resources/Models/')):
-#     filename = os.fsdecode(file)
-#     print(filename)
-#     model_name = filename.split(sep = '.')[0]
-#     print(model_name)
-#     model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
-#     model = joblib.load('Main/Resources/Models/' + filename)
-#     model_scores = supervised_ml.model_score(model)
-#     model_results['modelName'].append(model_list[key])
-#     model_results['modelPrediction'].append('y')
-#     model_results['modelAccuracy'].append(model_scores[0])
-#     model_results['modelF1'].append(model_scores[1])
-#     model_results['modelPrecision'].append(model_scores[2])
-#     model_results['modelRecall'].append(model_scores[3])
- 
-# print(model_results)
 
 def make_predictions(data):
 
@@ -38,41 +16,63 @@ def make_predictions(data):
                     'dmodelF1': [],
                     'emodelPrecision': [],
                     'fmodelRecall': []}
+ 
+    data['cap-diameter'] = float(data['cap-diameter'])
+    data['stem-height'] = float(data['stem-height'])
+    data['stem-width'] = float(data['stem-width'])
 
-    X = [] #15 features 
     print(data)
-    data['cap_diameter'] = float(data['cap_diameter'])
-    data['stem_height'] = float(data['stem_height'])
-    data['stem_width'] = float(data['stem_width'])
-    print(initial_data)
-    for feature in data.keys():
-        if '_' in feature:
-            renamed_feature = feature.replace('_', '-')
-            data[renamed_feature] = data.pop(feature)
-        else:
-            pass
+
+    # for feature in data.keys():
+    #     if '_' in feature:
+    #         renamed_feature = feature.replace('_', '-')
+    #         data[renamed_feature] = data.pop(feature)
+    #     else:
+    #         pass
 
     input_df = pd.DataFrame(data, index=[0])
-    # print(input_df)
     input_dummies = pd.get_dummies(input_df)
+
     print(input_dummies.columns)
     print(initial_data.columns)
+
     merged_df = initial_data.merge(input_dummies, how = 'outer')
+    merged_df.fillna(0.0, inplace = True)
+    input_data = merged_df.iloc[1, :].values
+
     print(merged_df)
+    print(merged_df.columns)
+    # print(input_data)
+    # print(len(input_data))
 
+    scaler = StandardScaler()
+    X_train = preprocessing()[1]
+    print(len(X_train))
+    scaler.fit(X_train)
+    # reshaped_data = np.array(input_data).reshape(-1, 1)
+    scaled_input_data = scaler.transform([input_data])
+    # if not os.path.exists('Main/Resources/Models'):
+    #     print("File Error")
+    # elif os.path.exists('Main/Resources/Models'):
+    #     print("You're dogwater")
+    # test = os.listdir('Main/Resources/Models')
+    # print(test)
 
-    for index, file in enumerate(os.listdir('Main/Resources/Models')):
+    for index, file in enumerate(os.listdir('static/models/')):
+        print(file)
         filename = os.fsdecode(file)
-        # model_name = filename.split(sep = '.')[0]
+        print(filename)
         model_list = ['Extreme Random Forest', 'K-Nearest Neighbor', 'Logistic Regression', 'Random Forest Classifier', 'SVM']
         model = joblib.load('Main/Resources/Models/' + filename)
-        # model.predict()
+        prediction = model.predict(scaled_input_data)
+        print(prediction)
         model_scores = supervised_ml.model_score(model)
+        model_scores = [float(score) for score in model_scores]
         model_results['amodelName'].append(model_list[index])
-        model_results['bmodelPrediction'].append('y')
+        model_results['bmodelPrediction'].append(float(prediction[0]))
         model_results['cmodelAccuracy'].append(model_scores[0])
         model_results['dmodelF1'].append(model_scores[1])
         model_results['emodelPrecision'].append(model_scores[2])
         model_results['fmodelRecall'].append(model_scores[3])
-
-    return model_results, data
+    print(model_results)
+    return model_results
